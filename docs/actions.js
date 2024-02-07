fdc4c8e035e0ded8b21a933b7500eec4a0f9ca55
renebec <39104197+renebec@users.noreply.github.com>
2024-02-07 11:03:25 -0600

    Add files via upload

---
+++ docs/actions.js
@@ -0,0 +1,78 @@
+      const btn = document.getElementById("button");
+
+btn.addEventListener("click", function(){
+var element = document.getElementById('body');
+html2pdf().from(element).save('filename.pdf');
+});
+
+function printFunction() {
+        window.print();
+      }
+
+function printFunction() {
+        window.print();
+      }
+
+
+function PopWinHome()
+{
+    window.open("index.html","width=100%", "_self");
+}
+
+
+function PopWinA1()
+{
+    window.open("popA1.html","width=100%", "_self");
+}
+
+
+function PopWinA1s1()
+{
+    window.open("popA1s1.html","width=100%", "_self");
+}
+
+
+function PopWinA1s1a()
+{
+    window.open("popA1s1a.html","width=100%", "_self");
+}
+
+function PopWinA1s1b()
+{
+    window.open("popA1s1b.html","width=100%", "_self");
+}
+
+
+function PopWinAr1()
+{
+    floatingWindow = window.open("popAr1.html","","height=400,width=400,scrollbars=yes");
+}
+
+function PopWinAr2()
+{
+    floatingWindow = window.open("popAr2.html","","height=400,width=400,scrollbars=yes");
+}
+
+function PopWinA1s1c()
+{
+    window.open("popA1s1c.html","width=100%", "_self");
+}
+
+function PopWinA1s1cE()
+{
+    window.open("popA1s1cE.html","","height=400,width=400,scrollbars=yes");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
